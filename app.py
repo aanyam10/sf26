40d700558376e468f11df1ad44697c39f86f7ad5
Praful Mendapara <pm@Prafuls-MacBook-Air.local>
2026-03-23 18:50:59 -0400

    Format subtitle to two lines and title case

--- app.py
+++ app.py
@@ -681,7 +681,7 @@ def main() -> None:
             <div style="text-align:center; color:#003366; font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; line-height:1.1; margin:0;">
                 <h1 style="margin:0; font-family:inherit; font-weight:700;">PulmoSense</h1>
                 <h3 style="margin:0.01rem 0 0 0; font-family:inherit; font-weight:600;">
-                    Using CNN and Neurosymbolic AI To Prevent Radiation-Induced Lung Injuries
+                    Using CNN And Neurosymbolic AI<br>To Prevent Radiation-Induced Lung Injuries
                 </h3>
                 <p style="margin:0.05rem 0 0 0; font-family:inherit; font-weight:500;">
                     WWSEF '26 - Aanya Mendapara
